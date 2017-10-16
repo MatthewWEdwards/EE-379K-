4945c142be56756ed3626ef8d282966e69a8a162
Tianyun Duan <leonduantian@hotmail.com>
2017-10-15 21:26:23 -0500

    question 1 and 3 completed

---
+++ lab6/question1.py
@@ -0,0 +1,50 @@
+###### Problem 1 ######
+
+### Part 1 ###
+import numpy as np
+from sklearn.linear_model import LinearRegression, Ridge
+
+# this block creates the matrix X with shape 51:50
+def get_X():
+    return np.random.normal(0,1,(51,50))
+
+# this block generates the beta (vector of all 1's)
+def get_b():
+    b = np.ndarray((50,1),float)
+    b.fill(1.0)
+    return b
+
+# this block creates the noise vector e with shape 51:1
+def get_e():
+    return np.random.normal(0,0.25,51).reshape(-1,1)
+
+lr = LinearRegression(fit_intercept=False)
+b_hats = []
+for i in range(100):
+    X = get_X()
+    b = get_b()
+    e = get_e()
+    y = np.dot(X,b) + e
+    lr.fit(X,y)
+    b_hats.append(lr.coef_)
+
+b_hats = np.asarray(b_hats)
+print b_hats.mean(axis=0).mean()
+print b_hats.var(axis=0).mean()
+
+
+### Part 2 ###
+for a in [0.01,0.1,1,10,100]:
+    ridge = Ridge(alpha=a)
+    b_hats = []
+    for i in range(100):
+        X = get_X()
+        b = get_b()
+        e = get_e()
+        y = np.dot(X,b) + e
+        ridge.fit(X,y)
+        b_hats.append(ridge.coef_)
+    b_hats = np.asarray(b_hats)
+    b_hat_avg = b_hats.mean(axis=0).flatten() # get the average of 100 solutions of b_hat
+    print np.linalg.norm(b_hat_avg,ord=1) # get the l1 norm to determin the model's complexity
+
---
+++ lab6/question3.py
@@ -0,0 +1,65 @@
+
+###### Problem 3 ######
+### Part a ###
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
+from sklearn.model_selection import cross_val_score
+
+
+df = pd.read_csv('./Boston.csv')
+
+y = df['crim']
+X = df[['zn','indus','chas','nox','rm','age','dis','rad','tax','ptratio','black','lstat','medv']]
+
+print y.shape
+print X.shape
+
+def mse_cv(model,X,y):
+    mse= -cross_val_score(model, X, y, scoring="neg_mean_squared_error", cv=kf_10)
+    return mse
+
+# lasso
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+
+model_lasso = linear_model.LassoCV(alphas = [0.001,0.01,0.1,1,10,100]).fit(X, y)
+mse_cv(model_lasso,X,y).mean()
+
+# ridge
+model_ridge = linear_model.RidgeCV(alphas = [0.001,0.01,0.1,1,10,100]).fit(X, y)
+mse_cv(model_ridge,X,y).mean()
+
+# pcr
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
+from sklearn import cross_validation
+from sklearn.linear_model import LinearRegression
+%matplotlib inline
+import seaborn as sns
+sns.set_style('darkgrid')
+
+X_reduced = pca.fit_transform(scale(X))
+np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100)
+
+n = len(X_reduced)
+kf_10 = cross_validation.KFold(n, n_folds=10, shuffle=True, random_state=2)
+
+regr = LinearRegression()
+mse = []
+
+score = -1*cross_validation.cross_val_score(regr, np.ones((n,1)), y.ravel(), cv=kf_10, scoring='neg_mean_squared_error').mean()    
+mse.append(score) 
+
+for i in np.arange(1,14):
+    score = -1*cross_validation.cross_val_score(regr, X_reduced[:,:i], y.ravel(), cv=kf_10, scoring='neg_mean_squared_error').mean()
+    print score
+    mse.append(score)
+
+fig, (ax1) = plt.subplots(1, figsize=(12,5))
+ax1.plot(mse, '-v')
+
+for ax in fig.axes:
+    ax.set_xlabel('Number of principal components in regression')
+    ax.set_ylabel('MSE')
+    ax.set_xlim((-0.2,13.2))
