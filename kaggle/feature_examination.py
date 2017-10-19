e7923ed2f2eb327dc7df79c8d6ee4d2b073d2d3f
MatthewWEdwards <mathew41796@gmail.com>
2017-10-19 11:10:44 -0500

    foo

--- kaggle/feature_examination.py
+++ kaggle/feature_examination.py
@@ -10,12 +10,24 @@ from scipy.stats.stats import pearsonr
 original_train_data = pd.read_csv("./train_final.csv")
 original_test_data = pd.read_csv("./test_final.csv")
 
-nan_vals = np.array([])
+train_nan_vals = np.array([])
+test_nan_vals = np.array([])
 for feat in range(1, 28):
-    nan_val = len(original_train_data[pd.isnull(original_train_data["F" + str(feat)])].index)
-    nan_vals = np.append(nan_vals, (nan_val))
-plt.plot(nan_vals)
+    train_nan_val = len(original_train_data[pd.isnull(original_train_data["F" + str(feat)])].index)
+    test_nan_val = len(original_test_data[pd.isnull(original_test_data["F" + str(feat)])].index)
+    train_nan_vals = np.append(train_nan_vals, (train_nan_val))
+    test_nan_vals = np.append(test_nan_vals, (test_nan_val))
+plt.plot(train_nan_vals)
+plt.title("NaN values in the training data by feature")
+plt.xlabel("Feature")
+plt.ylabel("NaN count")
 plt.show()    
+plt.plot(test_nan_vals)
+plt.title("NaN values in the testing data by feature")
+plt.xlabel("Feature")
+plt.ylabel("NaN count")
+plt.show()    
+
 
 
 
@@ -24,7 +36,14 @@ plt.show()
 
 #%%
 #Fill NaN
-train_data = original_train_data.fillna(original_train_data.mean())
+# There is about 1000 NaNs for F4 and 10000 for F18. The information excluded
+# from F18 is likely excluded based on the value of F18
+train_data = original_train_data.copy()
+train_data = train_data.drop(["id"], axis=1)
+train_data["F5"] = train_data["F5"].fillna(0) # 0 is the most frequent value
+train_data["F19"] = train_data["F19"].fillna(np.exp(8))
+# predict F18
+
 test_data = original_test_data.fillna(original_test_data.mean())
 
 #%% Get Features
@@ -35,7 +54,7 @@ test_features = test_data.copy()
 #Find some relationships between data
 cov =  np.corrcoef(train_features.loc[:, "F1":"F27"], rowvar=False)
 
-#Plot correlated relationships
+#%%Plot correlated relationships
 for row in range(0, 27):
     for col in range(row, 27):
         if (cov[row][col] > .3) & (row != col):
@@ -48,6 +67,17 @@ for row in range(0, 27):
             plt.show()
 
 
+#%% Scatter plots
+feat1 = range(1, 10)
+feat2 = range(10, 20)
+
+for f1 in feat1:
+    for f2 in feat2:
+        plt.scatter(train_features["F" + str(f1)], train_features["F" + str(f2)])
+        plt.xlabel("F" + str(f1))
+        plt.ylabel("F" + str(f2))
+        plt.show()
+
 #%% Get Outliers
 f2_outliers = train_features[train_features["F2"] > 10]
 f3_outliers = train_features[train_features["F3"] > 10]
@@ -55,6 +85,14 @@ f10_outliers = train_features[train_features["F10"] > 20]
 f14_outliers = train_features[train_features["F14"] > 20]
 f21_outliers = train_features[train_features["F21"] > 1000]
 f25_outliers = train_features[train_features["F25"] > 20]
+
+# Engineer some features
+train_features["F6"] = np.log(train_features["F6"]+1)
+train_features["F16"] = np.log(train_features["F16"]+1)
+train_features["F19"] = np.log(train_features["F19"]+1)
+train_features["F21"] = np.log(train_features["F21"]+1)
+train_features["F23"] = np.log(train_features["F23"]+1)
+
 
 #%% Plot histograms for train and test data
 for feat in range(1, 28):
@@ -75,3 +113,17 @@ y_likely = np.array([])
 for val in range(100, 180):
     y_likely = np.append(y_likely,train_y[train_features["F18"] == val].mean())
 plt.scatter(range(100, 180), y_likely)
+plt.show()
+
+#%%
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble.partial_dependence import plot_partial_dependence
+
+from sklearn.datasets import make_friedman1
+GBR = GradientBoostingRegressor(n_estimators=20).fit(train_features, train_y)
+plot_partial_dependence(GBR, train_features, range(1, 10)) 
+plt.show()
+plot_partial_dependence(GBR, train_features, range(10, 19)) 
+plt.show()
+plot_partial_dependence(GBR, train_features, range(19, 28)) 
+plt.show()
--- kaggle/models.py
+++ kaggle/models.py
@@ -14,15 +14,82 @@ import matplotlib.pyplot as plt
 from scipy.stats import skew
 from scipy.stats.stats import pearsonr
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import OneHotEncoder
+
 original_train_data = pd.read_csv("./train_final.csv")
 original_test_data = pd.read_csv("./test_final.csv")
 
 train_data = original_train_data.fillna(original_train_data.mean())
 test_data = original_test_data.fillna(original_test_data.mean())
 
+def round_col(x):
+    return round(x)
+
+#%% Grab importance features
+train_data = train_data[["Y", "F2", "F3", "F4", "F5", "F6", "F9", "F10", "F11", "F14", "F16", "F18", "F19", "F21", "F22", "F23", "F27"]]
+test_data = test_data[["F2", "F3", "F4", "F5", "F6", "F9", "F10", "F11", "F14", "F16", "F18", "F19", "F21", "F22", "F23", "F27"]]
+
+train_data["F5"] = train_data["F5"].fillna(0) # 0 is the most frequent value
+train_data["F19"] = train_data["F19"].fillna(np.exp(8))
+
+test_data["F5"] = test_data["F5"].fillna(0) # 0 is the most frequent value
+test_data["F19"] = test_data["F19"].fillna(np.exp(8))
+
+# One-hot encode
+train_data["F5"] = train_data["F5"].apply(round_col, axis=1)
+ohe = OneHotEncoder(categorical_features=[3, 4])
+train_data = ohe.fit_transform(train_data)	
+
+# log some feats
+train_data["F6"] = np.log(train_data["F6"]+1)
+train_data["F16"] = np.log(train_data["F16"]+1)
+train_data["F19"] = np.log(train_data["F19"]+1)
+train_data["F21"] = np.log(train_data["F21"]+1)
+train_data["F23"] = np.log(train_data["F23"]+1)
+#maybe 27?
+
+test_data["F6"] = np.log(test_data["F6"]+1)
+test_data["F16"] = np.log(test_data["F16"]+1)
+test_data["F19"] = np.log(test_data["F19"]+1)
+test_data["F21"] = np.log(test_data["F21"]+1)
+test_data["F23"] = np.log(test_data["F23"]+1)
+
+# Remove outliers
+train_data = train_data[train_data["F6"] < 9*(10**7)]
+train_data = train_data[train_data["F9"] < 3000]
+train_data = train_data[train_data["F16"] < 4000]
+train_data = train_data[train_data["F19"] < 200000]
+train_data = train_data[train_data["F21"] < 5000]
+train_data = train_data[train_data["F27"] < 30000]
+
+
+
 train_y = train_data["Y"]
-train_features = train_data.drop(["Y"], axis=1)
-test_features = test_data.copy()
+train_data = train_data.drop(["Y"], axis=1)
+
+
+
+#%% PCA etc.
+
+#from sklearn.decomposition import PCA
+#from sklearn.cross_decomposition import PLSRegression
+#pca = PLSRegression(n_components = 10)
+#pca.fit(train_data, train_y)
+#train_data = pca.transform(train_data)
+#test_data = pca.transform(test_data)
+
+
+
+#%%
+
 
 def roc_cv(model, x, y):
     roc = cross_val_score(model, x, y, scoring="roc_auc", cv = 5)
@@ -37,7 +104,6 @@ def conf_mat(model, x, y):
     return conf_mat
 
 
-
 def save_csv(model, x_train, y_train, x_test):
     model.fit(x_train, y_train)
     preds = pd.DataFrame()
@@ -45,42 +111,38 @@ def save_csv(model, x_train, y_train, x_test):
     preds["Y"] = XGB.predict(x_test)
     preds.to_csv(path_or_buf="./preds.csv", header=["id", "Y"], index=False)
     
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
 
+#%% XGB
 
-#%% Basic testing
-KNN = KNeighborsClassifier()
-GPC = GaussianProcessClassifier() # Causes memory error
-DTC = DecisionTreeClassifier()
-LR = LogisticRegression()
 XGB = xgb.XGBClassifier()
 
-models = [KNN, DTC, LR, XGB]
-for model in models:
-    print roc_cv(model, train_features, train_y)
-    print conf_mat(model,)
+parameters = {
+              'objective':['binary:logistic'],
+              'gamma': [.5], # Tested: .1, .2, .5, .7, 1, 2 Best=.5
+              'learning_rate': [.1], # Tested: 0.001, 0.002, 0.005, .1, .05, .1 Best=.1
+              'max_depth': [5], # Tested: 3, 5, 7, 9, 10 Best=5
+              'min_child_weight': [2], # Tested: 1,2,3,4,5 Best=2
+              'silent': [1], # Tested: 1 Best=1
+              'subsample': [.6], # Tested: .6, .7, .8 Best=.6
+              'colsample_bytree': [.8], # Tested: .6,.7,.8,.9,.10 Best=.8
+              'n_estimators': [25],  # Tested: 100 Best=100 (more should be better)
+              'reg_alpha': [1], # Tested: 0, .1, 1, 2, 5, 10 Best=1
+              'reg_lambda': [10] # Tested: 0, .1, 1, 10, 100 Best=10
+              
+              }
 
-#####Optomize on hyper parameters#####
 
-#%% KNN
-knn_vals = range(100,101) # Diminishing returns after 100 or so, peaks at .55
-cv_knn = [roc_cv(KNeighborsClassifier(n_neighbors=knn_val), train_features, train_y).mean() for knn_val in knn_vals]
-min_k = knn_vals[np.argmin(cv_knn)]
-cv_knn = pd.Series(cv_knn, index = cv_knn)
-plt.plot(range(2, 100), cv_knn)
-plt.title("KNN CV")
-plt.xlabel("K")
-plt.ylabel("ROC")
-plt.show()
+clf = GridSearchCV(XGB, parameters, n_jobs=1, cv=5, 
+                   scoring='roc_auc', refit=True)
+
+clf.fit(train_data, train_y)
+
+
 
 #%% DTC
 depth_vals = range(2,10)
-dtc_cv = [roc_cv(DecisionTreeClassifier(max_depth=depth_val), train_features, train_y).mean() for depth_val in depth_vals]
-min_depth = depth_vals[np.argmin(dtc_cv)]
+dtc_cv = [roc_cv(DecisionTreeClassifier(max_depth=depth_val), train_data, train_y).mean() for depth_val in depth_vals]
+max_depth = depth_vals[np.argmax(dtc_cv)]
 dtc_cv = pd.Series(dtc_cv, index = dtc_cv)
 plt.plot(range(2,10), dtc_cv)
 plt.title("DTC CV")
@@ -88,22 +150,86 @@ plt.xlabel("Depth")
 plt.ylabel("ROC")
 plt.show()
 
-#%% xgb
-XGB = xgb.XGBClassifier()
-print roc_cv(XGB, train_features, train_y).mean()
+#%% Random Classifier Forest
+# Get a good number of estimators
+est_vals = range(3, 20)
+rfc_cv = [roc_cv(RandomForestClassifier(n_estimators=depth_val), train_data, train_y).mean() for est_val in est_vals]
+max_n_est = est_vals[np.argmax(rfc_cv)]
+rfc_cv = pd.Series(rfc_cv, index = rfc_cv)
+plt.plot(range(3,20), rfc_cv)
+plt.title("RFC CV")
+plt.xlabel("Estimators Count")
+plt.ylabel("ROC")
+plt.show()
+max_n_est = 3 # Found using the above code. not optimal but close to it
+
+# Max Depth
+
+max_depth_vals = range(3, 20)
+rfc_cv = [roc_cv(RandomForestClassifier(n_estimators=3, max_depth=max_depth_val), train_data, train_y).mean() for max_depth_val in max_depth_vals]
+max_max_depth = est_vals[np.argmax(rfc_cv)]
+rfc_cv = pd.Series(rfc_cv, index = rfc_cv)
+plt.plot(range(3,20), rfc_cv)
+plt.title("RFC CV")
+plt.xlabel("Max Depth Val")
+plt.ylabel("ROC")
+plt.show()
+max_max_depth = 7 # Found using above code and optimal
+
+# Min sample split
+min_split_vals = [2, 5, 10, 20, 50, 100, 200, 500]
+rfc_cv = [roc_cv(RandomForestClassifier(n_estimators=3, max_depth=7, min_samples_split=min_split_val), train_data, train_y).mean() for min_split_val in min_split_vals]
+max_split_val = est_vals[np.argmax(rfc_cv)]
+rfc_cv = pd.Series(rfc_cv, index = rfc_cv)
+plt.plot([2, 5, 10, 20, 50, 100, 200, 500], rfc_cv)
+plt.title("RFC CV")
+plt.xlabel("Min Split Val")
+plt.ylabel("ROC")
+plt.show()
 
 
-#%% Tests
-XGB.fit(train_features, train_y)
-xgb_preds = XGB.predict(test_features)
-DTC = DecisionTreeClassifier(max_depth = 5)
-DTC.fit(train_features, train_y)
-dtc_preds = DTC.predict(test_features)
 
-preds = xgb_preds | dtc_preds
+#%% Enesmble models
 
+
+XGB = xgb.XGBClassifier(base_score=0.5, gamma=0.5, learning_rate=0.05,
+                        max_depth=5, min_child_weight=2, n_estimators=100, 
+                        reg_alpha = 1, reg_lambda=10, subsampled=0.6,
+                        colsample_bytree = .8)
+XGB.fit(train_data, train_y)
+xgb_train_preds = XGB.predict(train_data)
+xgb_test_preds = XGB.predict(test_data)
+
+
+DTC = DecisionTreeClassifier(max_depth = 4, min_samples_split = 40, criterion="entropy")
+DTC.fit(train_data, train_y)
+dtc_train_preds = DTC.predict(train_data)
+dtc_test_preds = DTC.predict(test_data)
+
+RFC = RandomForestClassifier(n_estimators=3, max_depth=7, min_samples_split=20)
+RFC.fit(train_data, train_y)
+rfc_train_preds = RFC.predict(train_data)
+rfc_test_preds = RFC.predict(test_data)
+
+super_log_reg_train = pd.DataFrame()
+super_log_reg_train["xgb"] = xgb_train_preds
+super_log_reg_train["dtc"] = dtc_train_preds
+super_log_reg_train["rfc"] = rfc_train_preds
+
+super_log_reg_test = pd.DataFrame()
+super_log_reg_test["xgb"] = xgb_test_preds
+super_log_reg_test["dtc"] = dtc_test_preds
+super_log_reg_test["rfc"] = rfc_test_preds
+
+LR_final = LogisticRegression()
+LR_final.fit(super_log_reg_train, train_y)
+print roc_cv(LR_final, super_log_reg_train, train_y)
+train_preds = LR_final.predict_proba(super_log_reg_train)[:,1]
+print roc_auc_score(train_y, train_preds)
+ensembled_preds = LR_final.predict_proba(super_log_reg_test)[:,1]
 #%%
 final_preds = pd.DataFrame()
-final_preds["id"] = test_features["id"]
-final_preds["Y"] = preds
+final_preds["id"] = original_test_data["id"]
+final_preds["Y"] = pd.Series(ensembled_preds, index=original_test_data.index)
 final_preds.to_csv(path_or_buf="./preds.csv", header=["id", "Y"], index=False)
+
